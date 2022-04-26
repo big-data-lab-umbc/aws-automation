4cb530ba871a1b5695a18b0372ade41426f7a0a9
Jianwu Wang <wangjianwu@gmail.com>
2022-04-26 11:20:47 -0400

    add mpi installation steps and horovod execution command

--- gpu-example/DL-4-Cloud-Retrieval/bootstrap_for_ami-078a38950439ea7bc.sh
+++ gpu-example/DL-4-Cloud-Retrieval/bootstrap_for_ami-078a38950439ea7bc.sh
@@ -3,12 +3,21 @@
 #ami-078a38950439ea7bc (64-bit (x86))
 #Built with AWS optimized TensorFlow, NVIDIA CUDA, cuDNN, NCCL, GPU Driver, Docker, NVIDIA-Docker and EFA support.
 
+# install open-mpi
+wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.0.tar.gz
+tar -xzvf openmpi-4.0.0.tar.gz
+cd openmpi-4.0.0/ && ./configure --prefix=/usr/local/ && sudo make all install
+
+# install horovod
 HOROVOD_WITH_PYTORCH=1 HOROVOD_GPU_OPERATIONS=NCCL HOROVOD_WITH_MPI=1 /usr/bin/python3.8 -m pip install --no-cache-dir horovod[pytorch,tensorflow,keras]
 
 #get and install code for cloud-phase-prediction-main
 wget -P /home/ec2-user/ https://ai-4-atmosphere-remote-sensing.s3.amazonaws.com/cloud-phase-prediction-main.zip
 unzip /home/ec2-user/cloud-phase-prediction-main.zip -d /home/ec2-user/
 cd /home/ec2-user/cloud-phase-prediction-main && /usr/bin/python3.8 -m pip install .
+
+#run horovod version
+cd /home/ec2-user/cloud-phase-prediction-main && horovodrun --verbose -np 1 -H localhost:1 /usr/bin/python3.8 train_para.py --training_data_path='./example/training_data/'  --model_saving_path='./saved_model/'
 
 #run commands on EC2 instance
 cd /home/ec2-user/cloud-phase-prediction-main && /usr/bin/python3.8 train.py --training_data_path='./example/training_data/'  --model_saving_path='./saved_model/'
